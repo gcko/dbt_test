d9c75e1f66870f0c9b4e445eba30159b6e2eae70
Jared Scott <jared.scott@variable.team>
2025-06-24 15:23:18 +0800

    Add recce file

--- models/staging/stg_purchases.sql
+++ models/staging/stg_purchases.sql
@@ -1,8 +1,13 @@
 with source as (SELECT *
                 FROM {{ ref('raw_purchases') }}),
 
-     renamed as (SELECT purchase_id, customer_id, store_id, purchase_date, amount
-FROM source)
+     renamed as (SELECT purchase_id,
+                        customer_id,
+                        store_id,
+                        purchase_date,
+                        -- divide by 100 (test)
+                        amount / 100 as amount
+                 FROM source)
 
 SELECT *
 FROM renamed
